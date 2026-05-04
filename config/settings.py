af73d60354de1419ccb8473d7f2a2d07177ebffa
okoriepeter62 <okoriepeter62@gmail.com>
2026-05-04 04:40:03 +0100

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -68,8 +68,9 @@ TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'staticfiles'  
-
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')  
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_URL = '/static/'
